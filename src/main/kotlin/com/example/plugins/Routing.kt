b0657173a0728ae658e871d713fdd72676a09ebc
Wael Ali Ghamdi <wael.ali.ghamdi@gmail.com>
2023-01-08 19:14:34 +0300

    clean up

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -3,11 +3,9 @@ package com.example.plugins
 import com.example.services.aboutRoute
 import com.example.services.conversationRoute
 import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
 
 fun Application.configureRouting() {
 
